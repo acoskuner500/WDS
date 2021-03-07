5bd6200c6a154503a3ca9086738d2b82bf5f21fb
Ahmet Coskuner <acoskuner500@tamu.edu>
2021-03-07 03:54:25 -0600

    Updated choose screen to send data to Firestore

--- app/src/main/java/com/example/wds/fragments/choose/ChooseFragment.kt
+++ app/src/main/java/com/example/wds/fragments/choose/ChooseFragment.kt
@@ -1,8 +1,6 @@
 package com.example.wds.fragments.choose
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.ImageButton
@@ -10,18 +8,11 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.wds.R
 import com.example.wds.databinding.FragmentChooseBinding
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ChooseFragment : Fragment(R.layout.fragment_choose) {
     private lateinit var binding: FragmentChooseBinding
     private lateinit var btnArray: Array<ImageButton>
-    private val prefs by lazy {
-        requireContext().getSharedPreferences(
-            prefsKey,
-            Context.MODE_PRIVATE
-        )
-    }
 
     companion object {
         private val animalKeys = arrayOf(
@@ -35,17 +26,10 @@ class ChooseFragment : Fragment(R.layout.fragment_choose) {
             "squirrel"
         )
         private val num = animalKeys.size
-        private const val prefsKey = "prefsKey"
-        private const val btnSelKey = "btnSelKey"
-        private const val btnEnKey = "btnEnKey"
-        private const val numSelKey = "numSelKey"
-        private const val TAG = "DEBUG"
+//        private const val TAG = "DEBUG"
     }
-    private var numSelected = 0
     private val oldSelected = Array(num) { false }
     private val newSelected = Array(num) { false }
-    private val oldEnabled = Array(num) { false }
-    private val newEnabled = Array(num) { false }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -55,60 +39,49 @@ class ChooseFragment : Fragment(R.layout.fragment_choose) {
                 btnBear, btnBoar, btnDeer, btnDog,
                 btnRabbit, btnRaccoon, btnSkunk, btnSquirrel
             )
-            loadData()
-            for (btn in btnArray) {
+            load()
+            for ((i,btn) in btnArray.withIndex()) {
                 btn.setOnClickListener {
-                    btnSelToggled(btn)
-                    btnEnToggled()
+                    val new = !btn.isSelected
+                    btn.isSelected = new
+                    newSelected[i] = new
                 }
             }
         }
     }
 
     override fun onPause() {
-        saveData()
+        save()
         super.onPause()
     }
 
-    private fun btnSelToggled(btn: ImageButton) {
-        btn.isSelected = !(btn.isSelected)
-        if (btn.isSelected) {
-            numSelected++
-        } else {
-            numSelected--
-        }
-    }
-
-    private fun btnEnToggled() {
-        for (btn in btnArray) {
-            if (numSelected >= num) {
-                btn.isEnabled = btn.isSelected
-            } else {
-                btn.isEnabled = true
+    private fun load() {
+        FirebaseFirestore.getInstance().collection("Verify")
+            .document("configuration").get()
+            .addOnSuccessListener {
+                for ((i, btn) in btnArray.withIndex()) {
+                    val sel = it.getBoolean(animalKeys[i])!!
+                    btn.isSelected = sel
+                    oldSelected[i] = sel
+                    newSelected[i] = sel
+                }
             }
-        }
+            .addOnFailureListener {
+                for (btn in btnArray) {
+                    btn.isSelected = false
+                }
+            }
     }
 
-    private fun saveData() {
-        for ((i,btn) in btnArray.withIndex()) {
-            newSelected[i] = btn.isSelected
-            newEnabled[i] = btn.isEnabled
-        }
-        val change = !newSelected.contentEquals(oldSelected) || !newEnabled.contentEquals(oldEnabled)
+    private fun save() {
         val text =
-            if (change) {
-                prefs.edit().apply {
-                    putInt(numSelKey, numSelected)
-                    for (btn in btnArray) {
-                        putBoolean(btnSelKey + btn.id.toString(), btn.isSelected)
-                        putBoolean(btnEnKey + btn.id.toString(), btn.isEnabled)
-                    }
-                }.apply()
+            if (!newSelected.contentEquals(oldSelected)) {
                 val dataHash = HashMap<String, Boolean>()
                 for ((i, sel) in newSelected.withIndex()) {
                     dataHash[animalKeys[i]] = sel
                 }
-                updateConfig(dataHash)
+                FirebaseFirestore.getInstance().collection("Verify")
+                    .document("configuration").update(dataHash as Map<String, Boolean>)
                 "Sending new configuration"
             } else {
                 "No change to configuration"
@@ -117,34 +90,5 @@ class ChooseFragment : Fragment(R.layout.fragment_choose) {
             setGravity(Gravity.BOTTOM,0,200)
             show()
         }
-    }
-
-    private fun loadData() {
-        numSelected = prefs.getInt(numSelKey, 0)
-        var sel = false
-        var en = true
-        for ((i,btn) in btnArray.withIndex()) {
-            if (numSelected != 0) {
-                sel = prefs.getBoolean(btnSelKey + btn.id.toString(), false)
-                en = prefs.getBoolean(btnEnKey + btn.id.toString(), true)
-            }
-            btn.isSelected = sel
-            btn.isEnabled = en
-            oldSelected[i] = sel
-            oldEnabled[i] = en
-            Log.d(TAG, "loadData: btn = ${btn.id}: sel = $sel, en = $en")
-        }
-    }
-
-    private fun updateConfig(data: HashMap<String, Boolean>) {
-        val configRef = Firebase.storage("gs://deterred/").reference
-            .child("configuration.json")
-        configRef.putBytes(data.toString().toByteArray())
-            .addOnFailureListener { e ->
-                Log.d(TAG, "updateConfig: $e")
-            }
-            .addOnSuccessListener {
-                Log.d(TAG, "updateConfig: configuration.json updated successfully")
-            }
     }
 }
